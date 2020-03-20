c98c4ab5f84deb105c791c30f4a678ab2e4e36c8
hasegawa282 <masaki.hasegawa@i-d.ai>
2020-03-21 07:56:25 +0900

    useStateがうまくいかない

--- src/views/templates/Home.tsx
+++ src/views/templates/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useCallback} from "react";
 import * as H from "history";
 import styled from "styled-components";
 
@@ -53,12 +53,22 @@ const Content = styled.div`
   height: calc(100% - ${headerHeight});
 `;
 
+const Asd=styled.div`
+  margin:0% 2%;
+`;
+
+const BorderLine=styled.div`
+  border-bottom:solid 1px black;
+`;
 
 
 const Home: React.FC = props => {
   // dispatch, state を用意
   const dispatch = useDispatch();
   const sampleState = useSelector(sampleSelector);
+  const [state, setState]=useState((<></>));
+  const [value, setValue]=useState("aiueo");
+  const [textName, setName]=useState("");
 
   const _handleClick = () => {
     dispatch(sampleModule.actions.changeNum(sampleState.someNum + 1));
@@ -66,8 +76,24 @@ const Home: React.FC = props => {
   const _handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(sampleModule.actions.changeStr(e.currentTarget.value));
   };
+
+  const textChange=(e:any)=>{
+    const newtextName=e.target.value;
+    setName(newtextName);
+  };
+
+  const submitText=()=>{
+    const newValue=textName;
+    setValue(newValue);
+    console.log(value);
+
+  }
+
+ 
+
   const clickChangeHome=()=>{
-    state.content=(
+    setState(
+      (
     <ContentBox>
       <TextLine>
         <div>ログインID:</div>
@@ -78,36 +104,46 @@ const Home: React.FC = props => {
         <SampleInput onChange={_handleChange} text={sampleState.someStr} />
       </ TextLine>
       <Button variant="light" style={{marginTop:"2%"}}>ログイン</Button>
-    </ContentBox>);
+    </ContentBox>)
+    );
   };
 
-  const clickChangeProfile=()=>{
-    state.content=(
-    <ContentBox>Welcome to the ID</ContentBox>);
+  const clickChangeBulletin=()=>{
+    setState(
+    <Asd>
+      <div>ユーザー名:<></></div>
+      <br />
+      <BorderLine>
+        <form>
+          <textarea  cols={80} rows={7} onChange={textChange}></textarea>
+          <br />
+          <input type="button" value="投稿" onClick={submitText}></input>
+      </form>
+      </BorderLine>
+        <div>
+          {value}
+        </div>
+    </Asd>
+    )
   };
 
-  const clickChangeLogin=()=>{
+  /*const clickChangeLogin=()=>{
     state.content=(
     <ContentBox>sign in</ContentBox>);
-  };
-
-  let state={
-    content:　<></>
-  };
-
+  };*/
 
     return (
       <Wrapper>
         <HeaderBox>
-        <div style={{fontSize:60}}>ID</div>
+        <div style={{fontSize:50}}>66チャンネル</div>
           <HeaderTextBox>
-           <a href="/" style={{marginRight:20 ,color:"#000"}} onClick={()=>clickChangeHome()}>Home</a>
-           <a href="/" style={{marginRight:20,color:"#000"}}>Profile</a>
-           <a href="/" style={{color:"#000"}}>Login</a>
+           <a href="#" style={{marginRight:20 ,color:"#000"}}>Home</a>
+           <a href="#" style={{marginRight:20,color:"#000"}} onClick={()=>{clickChangeBulletin()}}>掲示板</a>
+           <a href="#" style={{color:"#000"}} onClick={()=>clickChangeHome()}>Login</a>
           </HeaderTextBox>
         </HeaderBox>
         <Content>
-          {state.content}
+          {state}
         </Content>  
       </Wrapper>
     );
